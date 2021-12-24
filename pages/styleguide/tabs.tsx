35cd68aa2fd6177dfdbdfa7f6f8f80e8ebc83f9c
Jorrit Tempelman <tempelmanjorrit@gmail.com>
2021-12-24 14:44:25 +0100

    cleanup

--- pages/styleguide/tabs.tsx
+++ pages/styleguide/tabs.tsx
@@ -1,21 +1,14 @@
 import { Box } from '@components/Box'
 import { Container } from '@components/ui/Container'
-import { IconMoon } from '@components/ui/icons/IconMoon'
-import { IconSun } from '@components/ui/icons/IconSun'
-import { RadioGroup } from '@components/ui/radio-group'
-import { RadioGroupItem } from '@components/ui/radio-group/Item'
 import { Tabs } from '@components/ui/tabs'
-import { Text } from '@components/ui/typograhpy/Text'
 import { NextPage } from 'next'
 import React, { useState } from 'react'
 
 type StyleguideTabsPageProps = { children?: never }
 type FirstValue = 'layout' | 'size' | 'colors'
-type SecondValue = 'dark' | 'light'
 
 const StyleguideTabsPage: NextPage<StyleguideTabsPageProps> = () => {
   const [firstValue, setFirstValue] = useState<FirstValue>('layout')
-  // const [secondValue, setSecondValue] = useState<SecondValue>('dark')
   return (
     <Container mt="32">
       <Box mb="32" style={{ width: 'auto' }}>
